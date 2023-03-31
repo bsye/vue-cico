d93a327a1d306b687532bf10b3380feae1af8067
calum_haughton <calum.haughton@smartbox.com>
2023-03-31 17:43:46 +0100

    Added unit tests for DateInputs.vue

--- tests/unit/cico.DateInputs.spec.js
+++ tests/unit/cico.DateInputs.spec.js
@@ -2,16 +2,31 @@ import { mount } from '@vue/test-utils'
 import DateInputs from '../../components/DateInputs.vue'
 
 describe('DateInputs Component', () => {
-  const wrapper = mount(DateInputs, {})
+  const wrapper = mount(DateInputs, {
+    propsData: {
+      toggleDatepicker: () => true,
+      i18n: {}, 
+      checkIn:null
+    }
+  })
 
-  it('should verify the inputWidth', async () => {
+  it('should update the inputSize based on the inputWidth', async () => {
     wrapper.vm.inputWidth = 400
+    expect(wrapper.vm.inputSize).toBe('long')
 
-    wrapper.vm.inputWidth = 250
+    wrapper.vm.inputWidth = 234
+    expect(wrapper.vm.inputSize).toBe('short')
 
-    wrapper.vm.inputWidth = 200
+    wrapper.vm.inputWidth = 149
+    expect(wrapper.vm.inputSize).toBe('extra-short')
 
-    console.log(wrapper.vm.inputWidth)
-    console.log(wrapper.vm.inputSize)
+  })
+
+  it('should show the extended placeholder if the input is long and no selection is made', () => {
+    wrapper.vm.inputWidth = 1
+    expect(wrapper.vm.showExtendedPlaceholder).toBe(false)
+
+    wrapper.vm.inputWidth = 400
+    expect(wrapper.vm.showExtendedPlaceholder).toBe(true)
   })
 })
