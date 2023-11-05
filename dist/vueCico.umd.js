0555223daca35fa10e7f4892df910d623943d680
Alberto Dal Bianco <alberto.dalbianco@smartbox.com>
2023-11-05 20:35:08 +0000

    Updated dist

--- dist/vueCico.umd.js
+++ dist/vueCico.umd.js
@@ -2730,7 +2730,7 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"5ee13767-vue-loader-template"}!./node_modules/cache-loader/dist/cjs.js??ref--13-0!./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Cico.vue?vue&type=template&id=30a2c674&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"5ee13767-vue-loader-template"}!./node_modules/cache-loader/dist/cjs.js??ref--13-0!./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Cico.vue?vue&type=template&id=7d6ed272&
 var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
@@ -2948,7 +2948,7 @@ var render = function render() {
 };
 var staticRenderFns = [];
 
-// CONCATENATED MODULE: ./components/Cico.vue?vue&type=template&id=30a2c674&
+// CONCATENATED MODULE: ./components/Cico.vue?vue&type=template&id=7d6ed272&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.push.js
 var es_array_push = __webpack_require__("14d9");
@@ -5369,7 +5369,7 @@ var CallToAction_component = normalizeComponent(
       }
     },
     mobileActionSelected() {
-      this.isOpen = !this.isOpen;
+      this.toggleDatepicker();
       this.$emit('search-mobile-triggered');
     },
     disabledDateIsCheckIn() {
